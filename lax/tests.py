21d2ab0bfd97a087167efdf21b0cd747bf98b299
Shavolski <successismyname101@gmail.com>
2018-07-23 18:20:52 +0300

    fixed bugs on the test files

--- lax/tests.py
+++ lax/tests.py
@@ -2,11 +2,12 @@
 from __future__ import unicode_literals
 from . models import Post, Profile
 from django.test import TestCase
+from django.contrib.auth.models import User
 
 class ImageTestClass(TestCase):
     def setUp(self):
-        self.user = User.objects.create_user(username: '', password: '')
+        self.user = User.objects.create_user('','')
         self.image = Image.objects.create(image_link='posts/', name='',caption='',created_by=self.user)
     def test_instance(self):
         self.image.save()
-        self.assertTrue(isinstance(self.image, Image))    
+        self.assertTrue(isinstance(self.Post,Post))
