9633d7d9b0ede15970b6cc18ffd41bcdbf4afe55
Shavolski <successismyname101@gmail.com>
2018-07-23 09:06:57 +0300

    generated forms.py with defined classes

---
+++ lax/forms.py
@@ -0,0 +1,18 @@
+from django import forms
+from .models import Profile,Post
+from django.contrib.auth.models import User
+
+class UserForm(forms.ModelForm):
+  class Meta:
+    model = User
+    fields = ('first_name', 'last_name', 'email')
+
+class ProfileForm(forms.ModelForm):
+  class Meta:
+    model = Profile
+    fields = ('bio', 'location','birth_date','website','phone_number','profile_pic')
+
+class PostForm(forms.ModelForm):
+  class Meta:
+    model = Post
+    fields = ('title', 'image') 
