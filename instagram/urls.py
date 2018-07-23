b95e63ee2e48d2d2dddb180af9a49e7b0b3dff7d
Shavolski <successismyname101@gmail.com>
2018-07-23 07:19:49 +0300

    defined urls in both project and app

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,5 +18,5 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
-    url(r'^lax/',include('lax.urls'))
+    url(r'',include('lax.urls'))
 ]
--- lax/urls.py
+++ lax/urls.py
@@ -1,9 +1,11 @@
 from django.conf.urls import url
+from django.conf.urls.static import static
+from django.conf import settings
 from . import views
 
 urlpatterns=[
-    url('^$',views.homepage,name ='homepage'),
-    url('^$',views.profilepage,name ='profilepage'),
+    url('^$',views.homepage,name ='home'),
+    url('^profile/(?P<username>[-_\w.]+)/$',views.profilepage,name ='profile'),
     url('^$',views.discoverpage,name ='discoverpage'),
     url('^$',views.searchpage,name ='searchpage'),
     url('^$',views.registerationpage,name ='registerationpage'),
