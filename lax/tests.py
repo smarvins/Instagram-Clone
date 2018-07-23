558d6ad532c1a4434b924d2600401deb5029d5b9
Shavolski <successismyname101@gmail.com>
2018-07-23 18:11:08 +0300

    typed test cases for the profile class

--- lax/tests.py
+++ lax/tests.py
@@ -1,6 +1,12 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
-
+from . models import Post, Profile
 from django.test import TestCase
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.user = User.objects.create_user(username: '', password: '')
+        self.image = Image.objects.create(image_link='posts/', name='',caption='',created_by=self.user)
+    def test_instance(self):
+        self.image.save()
+        self.assertTrue(isinstance(self.image, Image))    
