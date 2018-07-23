a4d29a508c7316bfa0f0c1d9012d79ace97a1cd4
Shavolski <successismyname101@gmail.com>
2018-07-23 21:08:51 +0300

    FInal function to like an image

--- lax/views.py
+++ lax/views.py
@@ -125,3 +125,27 @@ def likes(request, pk):
   title = 'Likes'
   return render(request, 'follow_list.html', {"title": title, "profiles":profiles})
 
+######################################################################
+
+
+'''Here's where you can add a like to an image'''
+@ajax_request
+@login_required
+def add_like(request):
+    post_pk = request.POST.get('post_pk')
+    post = Post.objects.get(pk=post_pk)
+    try:
+        like = Like(post=post, user=request.user)
+        like.save()
+        result = 1
+    except Exception as e:
+        like = Like.objects.get(post=post, user=request.user)
+        like.delete()
+        result = 0
+
+    return {
+        'result': result,
+        'post_pk': post_pk
+    }
+
+
